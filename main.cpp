5ba079c5dc8b9513d333b47c98163c45d90737b8
SERGEY MIRONOV <122725516+smironov83@users.noreply.github.com>
2023-07-16 19:52:09 +0500

    Упрощение кода

--- main.cpp
+++ main.cpp
@@ -5,7 +5,7 @@
 const long N = 100000000;
 const int M = 10;
 long long result = 0;
-int array[N] {};
+int *array = new int[N] {};
 
 void CalcOutTreads(long first, long last)
 {
@@ -21,10 +21,10 @@ auto main() -> int
   system("cls");
   srand((unsigned int) time(0));
   for (auto i = 0; i < N; ++i)
-   array[i] = rand() % 10000;
-  
+    array[i] = rand() % 1000000000;
   auto timeStart = std::chrono::system_clock::now();
-  CalcOutTreads(0, N);
+  for (auto i = 0; i < M; ++i)
+    CalcOutTreads(N / M * i, N / M * (i + 1));
   auto timeFinish = std::chrono::system_clock::now();
   auto timeSpent = std::chrono::duration_cast<std::chrono::milliseconds>
     (timeFinish - timeStart).count();
@@ -32,21 +32,18 @@ auto main() -> int
     " без использования потоков за " << timeSpent << " мс." << std::endl;
   timeStart = std::chrono::system_clock::now();
   result = 0;
-  std::thread threads[M] {};
+  std::thread *threads = new std::thread[M] {};
   for (auto i = 0; i < M; ++i)
-  {
-    std::thread t(CalcOutTreads, N / M * i, N / M * (i + 1));
-    threads[i] = std::move(t);
-  }
-  for (auto &t : threads)
-  {
-    if (t.joinable())
-      t.join();
-  }
+    threads[i] = std::thread(CalcOutTreads, N / M * i, N / M * (i + 1));  
+  for (auto i = 0; i < M; ++i)
+    if (threads[i].joinable())
+      threads[i].join();
   timeFinish = std::chrono::system_clock::now();
   timeSpent = std::chrono::duration_cast<std::chrono::milliseconds>
     (timeFinish - timeStart).count();
   std::cout << "Сумма всех элементов массива равна " << result << ", вычислена"
     " с использованием потоков за " << timeSpent << " мс." << std::endl;
+  delete[]array;
+  delete[]threads;
   return 0;
 }
